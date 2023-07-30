c51e153a6389f5ee6ded0c35906c4d4cf2529cfa
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-07-30 13:12:28 -0300

    Criação da exceção AuthorNullException

---
+++ src/br/com/LibraryNostra/livraria/exception/AuthorNullException.java
@@ -0,0 +1,9 @@
+package br.com.LibraryNostra.livraria.exception;
+
+public class AuthorNullException extends RuntimeException {
+
+	public AuthorNullException(String message) {
+		super(message);
+	}
+
+}
