e9a8897fe8c073fb11142d2fc3cfa2980cb4a5ad
Felipe Rocha dos Santos <99603179+FRSantos-Dev@users.noreply.github.com>
2023-07-30 12:59:57 -0300

    Criação da Classe Author

---
+++ src/br/com/LibraryNostra/livraria/Author.java
@@ -0,0 +1,47 @@
+package br.com.LibraryNostra.livraria;
+
+public class Author {
+
+	private String name;
+	private String email;
+	private String cpf;
+	
+	public void showDetails() {
+		System.out.println("Mostrando detalhes do autor ");
+		System.out.println("Nome: " + name);
+		System.out.println("Email: " + email);
+		System.out.println("CPF: " + cpf);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (!(obj instanceof Author)) return false;
+		Author other = (Author) obj;
+		return this.name.equals(other.name);
+	}
+	
+}
